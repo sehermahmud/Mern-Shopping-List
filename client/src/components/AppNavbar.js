63e25e870c2d2db5927017aae952264c8f0d5024
sehermahmud <seher.mahmud@gmail.com>
2020-06-07 18:18:26 +0600

    Version 1.0.0

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="danger" dark expand="sm" className="mb-5" >
+                    <Container>
+                        <NavbarBrand href="/"><h2>ShoppingList</h2></NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://react-beach-resort-recording-seher.netlify.app">react-BeachResort</NavLink>
+                                    <NavLink href="https://react-phone-store-recording-seher.netlify.app">react-PhoneStore</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavbar;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://seher:seher123@cluster0-v4fca.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
